5581ac6e95c5b94d17010d0691393b37c54c6b79
Sonph <son.ph@anlab.biz>
2015-04-01 14:38:03 +0700

    sfsdfsdf

--- app/assets/javascripts/website/home.js
+++ app/assets/javascripts/website/home.js
@@ -2,4 +2,9 @@ jQuery( function($){
   $("#user_sign_in_button").on('click', function(e) {
     $("#profile_user").slideToggle();
   });
+  $("#custom_user_dropdown li").hover(function() {
+    $(this).addClass('active');
+  }, function(){
+    $(this).removeClass('active');
+  });
 });
