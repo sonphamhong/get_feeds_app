9205cc026c8d801c4b3e5013c3abe70e0a6c27b4
Sonph <evilsp4@gmail.com>
2015-04-01 20:17:09 +0700

    sdfsdf

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,7 +1,7 @@
 class Admin::HomeController < AdminController
   def index
-    @graph = Koala::Facebook::API.new("CAACEdEose0cBAPZAwIx2ArPoFsvdyFeCNZBEEkuZBvlXe77DY0m9vusxDpnXp9k0aLSdOj4RUl5WgyZCqgDZAPi7UvCtWOwS1O6ZCDsAx1fzJ0jZBB3FCTGaJfKFMrWLFHJzDtk3aCmwqbdgZBnlOQgNQKFUopoG2wP96AwYSGCk3W2L8t0PUdkgHmKT2dOIdLSnRziJvB5js5ZCKd1uZCSnhlCd7vPudAnhYZD")
-    feeds = @graph.get_connections("511088052277392", "?fields=feed.limit(100){message,link,picture}")
-    binding.pry
+    # @graph = Koala::Facebook::API.new("CAACEdEose0cBAPZAwIx2ArPoFsvdyFeCNZBEEkuZBvlXe77DY0m9vusxDpnXp9k0aLSdOj4RUl5WgyZCqgDZAPi7UvCtWOwS1O6ZCDsAx1fzJ0jZBB3FCTGaJfKFMrWLFHJzDtk3aCmwqbdgZBnlOQgNQKFUopoG2wP96AwYSGCk3W2L8t0PUdkgHmKT2dOIdLSnRziJvB5js5ZCKd1uZCSnhlCd7vPudAnhYZD")
+    # feeds = @graph.get_connections("511088052277392", "?fields=feed.limit(100){message,link,picture}")
+    # binding.pry
   end
 end
